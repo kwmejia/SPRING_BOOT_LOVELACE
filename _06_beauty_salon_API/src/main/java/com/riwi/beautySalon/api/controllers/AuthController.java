a5d56f3ccd57f729d190644db6e12d1760170564
Kevin Mejia <kwmejia9@gmail.com>
2024-05-10 12:57:35 -0500

    10-05-2024 Entity User

--- _06_beauty_salon_API/src/main/java/com/riwi/beautySalon/api/controllers/AuthController.java
+++ _06_beauty_salon_API/src/main/java/com/riwi/beautySalon/api/controllers/AuthController.java
@@ -1,12 +1,26 @@
 package com.riwi.beautySalon.api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.riwi.beautySalon.api.dto.request.RegisterReq;
+import com.riwi.beautySalon.api.dto.response.AuthResp;
+import com.riwi.beautySalon.infraestructure.abstract_service.IAuthService;
+
+import lombok.AllArgsConstructor;
+
 @RestController
 @RequestMapping
+@AllArgsConstructor
 public class AuthController {
+
+    @Autowired
+    private final IAuthService authService;
     
     @PostMapping(path = "/auth/login")
     public String login(){
@@ -14,7 +28,9 @@ public class AuthController {
     }
 
     @PostMapping(path = "/auth/register")
-    public String register(){
-        return "DESDE REGISTER";
+    public ResponseEntity<AuthResp> register(
+       @Validated @RequestBody RegisterReq request 
+    ){
+        return ResponseEntity.ok(this.authService.register(request));
     }
 }
