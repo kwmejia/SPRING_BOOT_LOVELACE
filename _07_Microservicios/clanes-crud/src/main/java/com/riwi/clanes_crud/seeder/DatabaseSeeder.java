540bad41777e0c01204a5b1baad5d2386ef03202
Kevin Mejia <kwmejia9@gmail.com>
2024-07-17 10:00:38 -0500

    create seed

--- _07_Microservicios/clanes-crud/src/main/java/com/riwi/clanes_crud/seeder/DatabaseSeeder.java
+++ _07_Microservicios/clanes-crud/src/main/java/com/riwi/clanes_crud/seeder/DatabaseSeeder.java
@@ -1,5 +1,34 @@
 package com.riwi.clanes_crud.seeder;
 
-public class DatabaseSeeder  {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.riwi.clanes_crud.entities.Cohort;
+import com.riwi.clanes_crud.repositories.CohortRepository;
+
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+public class DatabaseSeeder implements CommandLineRunner {
+
+    @Autowired
+    private final CohortRepository cohortRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        
+        if (this.cohortRepository.count() > 0) return;            
+     
+
+        Cohort cohort1 = Cohort.builder().name("Cohort 1").build();
+        Cohort cohort2 = Cohort.builder().name("Cohort 2").build();
+        Cohort cohort3 = Cohort.builder().name("Cohort 3").build();
+
+        this.cohortRepository.save(cohort1);
+        this.cohortRepository.save(cohort2);
+        this.cohortRepository.save(cohort3);
+    }
     
 }
